587e7d0e3dd577e2dd8b90366c12b06c5ef0c89c
yoppi <y.hirokazu@gmail.com>
2011-08-01 18:09:13 +0900

    added instance valiables

--- lib/backlog/client.rb
+++ lib/backlog/client.rb
@@ -1,8 +1,14 @@
 # coding: utf-8
 
+require 'xmlrpc/client'
+
 module Backlog
   class Client
-    def initialzie
+    def initialize(project_name, username, password)
+      @project_name = project_name
+      @username = username
+      @password = password
     end
+    attr_reader :project_name, :username, :password
   end
 end
--- spec/backlog/client_spec.rb
+++ spec/backlog/client_spec.rb
@@ -4,9 +4,23 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe Backlog::Client do
   context "特定のユーザのプロジェクトでAPIにアクセスする" do
+    let(:project_name) { 'test' }
+    let(:username) { 'yoppi' }
+    let(:password) { 'hoge' }
+    let(:client) {
+      Backlog::Client.new(project_name, username, password)
+    }
+
     it "Backlogにアクセスするクライアントオブジェクトが生成されること" do
-      client = Backlog::Client.new()
       client.should_not be_nil
+    end
+
+    it "ユーザ名が取得できること" do
+      client.username.should == username
+    end
+
+    it "プロジェクト名が取得できること" do
+      client.project_name.should == project_name
     end
   end
 end
