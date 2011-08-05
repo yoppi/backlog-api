d221ea2c28da73078b304667b5c9a478d5f5c022
yoppi <y.hirokazu@gmail.com>
2011-08-05 16:15:53 +0900

    added Double Ruby declare. to use spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
+require 'rr'
+
 require 'backlog'
 
 # Requires supporting files with custom matchers and macros, etc,
@@ -8,5 +10,5 @@ require 'backlog'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  config.mock_with :rr
 end
